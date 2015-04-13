7e80077a5cc9ed5fad3c5c86b7c802edead2dd1e
DeShawn Cousette <dcousette@gmail.com>
2015-04-12 23:44:02 -0400

    Add associations for Post and Category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411133518) do
+ActiveRecord::Schema.define(version: 20150412150601) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20150411133518) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer "post_id"
+    t.integer "category_id"
   end
 
   create_table "posts", force: true do |t|
